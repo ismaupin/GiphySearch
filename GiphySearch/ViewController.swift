4fbfeb1fe572874403a7fecf73e0d0ba42f46d15
Isaac Maupin <Isaac.Maupin@gmail.com>
2020-08-13 14:13:20 -0400

    Initial Commit

---
+++ GiphySearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GiphySearch
+//
+//  Created by Isaac Maupin on 8/13/20.
+//  Copyright © 2020 Isaac Maupin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
